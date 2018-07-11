ee8d3dcdad30db7aec2b997ab583078a96d6c87c
Pedro Baracho <pedropb@trt3.jus.br>
2018-07-11 10:25:15 -0300

    Finished scaffolding

--- background.js
+++ background.js
@@ -3,21 +3,22 @@ chrome.runtime.onInstalled.addListener(function() {
   var CURRENCY_FROM = "CAD";
   var CURRENCY_TO = "BRL";
   var URL = "http://free.currencyconverterapi.com/api/v3/convert?compact=ultra&q=" + CURRENCY_FROM + "_" + CURRENCY_TO;
+  var TITLE = "CAD -> BRL";
 
   function updateBadge () {
+    chrome.browserAction.setBadgeText({ text: "..." });
     var xhr = new XMLHttpRequest();
     xhr.open("GET", URL, true);
     xhr.onreadystatechange = function(data){
         if (xhr.readyState == 4) {
-          console.log('received');
-          console.log(data);
           try {
             var value = JSON.parse(data.target.responseText)[CURRENCY_FROM + "_" + CURRENCY_TO];
             chrome.browserAction.setBadgeText({ text: value.toFixed(2).toString() });
           }
           catch (ex) {
-            chrome.browserAction.setBadgeText({ text: 'Err'});
+            chrome.browserAction.setBadgeText({ text: "Err" });
           }
+          chrome.browserAction.setTitle({ title: TITLE });
         }
     }
     xhr.send();
