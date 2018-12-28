4ef3e92ab9279809ea918fc26b496f5853974a18
Pedro Baracho <pedropbaracho@gmail.com>
2018-12-28 09:58:52 -0600

    Change build script to minify files
    Right now building the project requires uglify to be installed on user PATH.
    Also updated promotional and screenshot images to feature the correct icons.

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 rm dist.zip
-zip dist.zip background.js LICENSE manifest.json options.html options.js screenshot.png icons/icon16.png icons/icon48.png icons/icon128.png
+uglify -s background.js -o background.min.js
+uglify -s options.js -o options.min.js
+zip dist.zip background.min.js LICENSE manifest.json options.html options.min.js icons/icon16.png icons/icon48.png icons/icon128.png
